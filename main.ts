73c4c55c7b80cc015c88955e81a67c17d4fc190b
TheDolentCity <nardogamedev@outlook.com>
2023-10-28 19:49:21 -0700

    Initial commit

--- main.ts
+++ main.ts
@@ -1,8 +1,2 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const webhookId = Deno.env.get("DISCORD_WEBHOOK_ID");
+const webhookToken = Deno.env.get("DISCORD_WEBHOOK_TOKEN");
